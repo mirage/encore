d4c6e3013ad57cffb428993f8cd6e96fd54103d5
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-03-14 00:29:13 +0100

    Add interface of Encore

---
+++ lib/encore.mli
@@ -0,0 +1,11 @@
+module Meta = Meta
+
+module Bijection = Bijection
+
+module FQueue = FQueue
+
+module Lole = Lole
+module Encoder = Encoder
+
+module Proxy_decoder = Proxy_decoder
+module Proxy_encoder = Proxy_encoder
